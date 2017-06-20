babae5a61b6c52f08c32c16f58adb95c5ea9cc2a
Syu125 <yusophia2011@gmail.com>
2017-06-19 18:48:55 -0700

    Day 1

--- src/demos/LightSensorDemo.java
+++ src/demos/LightSensorDemo.java
@@ -41,27 +41,45 @@ public class LightSensorDemo extends IRobotAdapter {
 		//the higher the integer the closer the object
 	
 		driveDirect(500,500);
-		if(lightBumpReadings[5] > 0){
+		if(lightBumpReadings[3]>0){
+			driveDirect(0,0);
+			sleep(1000);
+			driveDirect(-300,-300);
+			sleep(1000);
+			driveDirect(0,500);
+			sleep(1500);
+			driveDirect(350,30);
+		}else if(lightBumpReadings[2]>0){
+			driveDirect(0,0);
+			sleep(1000);
+			driveDirect(-300,-300);
+			sleep(1000);
+			driveDirect(0,500);
+			sleep(1500);
+			driveDirect(500,500);
+		}
+	
+		if(lightBumpReadings[0] > 0){
 			driveDirect(130, 100);
 			sleep(500);
 			driveDirect(0, 0);
-		}else if(lightBumpReadings[4] > 0){
+		}else if(lightBumpReadings[1] > 0){
 			driveDirect(160, 100);
 			sleep(500);
 			driveDirect(0, 0);
-		}else if(lightBumpReadings[3] > 0){
+		}else if(lightBumpReadings[2] > 0){
 			driveDirect(180, 100);
 			sleep(500);
 			driveDirect(0, 0);
-		}else if(lightBumpReadings[2] > 0){
+	}else if(lightBumpReadings[3] > 0){
 			driveDirect(100, 180);
 			sleep(500);
 			driveDirect(0, 0);
-		}else if(lightBumpReadings[1] > 0){
+		}else if(lightBumpReadings[4] > 0){
 			driveDirect(100, 160);
 			sleep(500);
 			driveDirect(0, 0);
-		}else if(lightBumpReadings[0] > 0){
+		}else if(lightBumpReadings[5] > 0){
 			driveDirect(10, 130);
 			sleep(500);
 			driveDirect(0, 0);
