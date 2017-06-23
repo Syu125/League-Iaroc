04c4873eb6b8af3df2b8a7c0689b28b7001d9e6e
Syu125 <yusophia2011@gmail.com>
2017-06-22 17:50:55 -0700

    Competition Code

---
+++ src/demos/MazeRunnerMazeFace.java
@@ -0,0 +1,68 @@
+package demos;
+
+import java.io.IOException;
+
+import org.jointheleague.ecolban.rpirobot.IRobotAdapter;
+import org.jointheleague.ecolban.rpirobot.IRobotInterface;
+import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
+
+public class MazeRunnerMazeFace extends IRobotAdapter {
+	//Sonar sonar = new Sonar();
+	
+	public MazeRunnerMazeFace(IRobotInterface iRobot) {
+		super(iRobot);
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Try event listner, revn Monday 2030");
+		IRobotInterface base = new SimpleIRobot();
+		MazeRunnerMazeFace rob = new MazeRunnerMazeFace(base);
+		rob.setup();
+		while(rob.loop()){}
+		rob.shutDown();	
+	}
+	
+	private void setup() throws Exception {
+		//SETUP CODE GOES HERE!!!!!
+	}
+	
+	private boolean loop() throws Exception{
+		//LOOP CODE GOES HERE!!!!!
+		readSensors(100);
+		//int[] lightBumpReadings = getLightBumps();
+		driveDirect(500, 178);
+		//driveDirect(500,350);
+		if(isBumpRight()){
+			driveDirect(-230, 280);
+			sleep(200);
+			
+		}
+		if(isBumpRight( ) && isBumpLeft()){
+			driveDirect(-250,-250);
+			sleep(500);
+		}
+		if(isBumpLeft()){
+			  driveDirect(-500,-500);
+			  sleep(100);
+			  driveDirect(50,500);
+			  sleep(300);
+		}
+		
+		return true;
+	}
+
+	private void sleep(int amt){
+		try {
+			Thread.sleep(amt);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void shutDown() throws IOException {
+		reset();
+		stop();
+		closeConnection();
+	}
+}
---
+++ src/demos/SpeedyDerpMazeFace.java
@@ -0,0 +1,118 @@
+package demos;
+
+import java.io.IOException;
+
+import org.jointheleague.ecolban.rpirobot.IRobotAdapter;
+import org.jointheleague.ecolban.rpirobot.IRobotInterface;
+import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
+
+public class SpeedyDerpMazeFace extends IRobotAdapter {
+	//Sonar sonar = new Sonar();
+	
+	public SpeedyDerpMazeFace(IRobotInterface iRobot) {
+		super(iRobot);
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Try event listner, rev Monday 2030");
+		IRobotInterface base = new SimpleIRobot();
+		SpeedyDerpMazeFace rob = new SpeedyDerpMazeFace(base);
+		rob.setup();
+		while(rob.loop()){}
+		rob.shutDown();		
+	}
+
+	private void setup() throws Exception {
+		
+	}
+	
+	private boolean loop() throws Exception{
+		readSensors(100);
+		
+		//getLightBumps() returns an array of length 6 (elements 0 - 5)
+		//element 0 is the far left light sensor
+		//element 5 is the far right light sensor
+		//elements 1-4 are the sensors in between from left to right
+		
+		int[] lightBumpReadings = getLightBumps();
+		
+		//if the sensors don't detect anything, they return 0
+		//otherwise they return some positive integer
+		//the higher the integer the closer the object
+	
+		driveDirect(1000,1000);
+		/*if(lightBumpReadings[3]>0){
+			driveDirect(0,0);
+			sleep(1000);
+			driveDirect(-300,-300);
+			sleep(1000);
+			driveDirect(0,500);
+			sleep(1500);
+			driveDirect(1000,1000);
+		}else if(lightBumpReadings[2]>0){
+			driveDirect(0,0);
+			sleep(1000);
+			driveDirect(-300,-300);
+			sleep(1000);
+			driveDirect(0,500);
+			sleep(1500);
+			driveDirect(1000,1000);
+		}
+		*/
+	
+		if(lightBumpReadings[0] > 0){
+			driveDirect(-500,-500);
+			sleep(400);
+			driveDirect(360, 20);
+			sleep(400);
+			driveDirect(0, 0);
+		}else if(lightBumpReadings[1] > 0){
+			driveDirect(-500,-500);
+			sleep(400);
+			driveDirect(380, 20);
+			sleep(400);
+			driveDirect(0, 0);
+		}else if(lightBumpReadings[2] > 0){
+			driveDirect(-500,-500);
+			sleep(400);
+			driveDirect(400, 20);
+			sleep(400);
+			driveDirect(0, 0);
+	}else if(lightBumpReadings[3] > 0){
+		driveDirect(-500,-500);
+			sleep(400);
+			driveDirect(20, 400);
+			sleep(400);
+			driveDirect(0, 0);
+		}else if(lightBumpReadings[4] > 0){
+			driveDirect(-500,-500);
+			sleep(400);
+			driveDirect(20, 380);
+			sleep(400);
+			driveDirect(0, 0);
+		}else if(lightBumpReadings[5] > 0){
+			driveDirect(-500,-500);
+			sleep(400);
+			driveDirect(20, 360);
+			sleep(400);
+			driveDirect(0, 0);
+		}
+		
+		return true;
+	}
+	
+	private void sleep(int amt){
+		try {
+			Thread.sleep(amt);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void shutDown() throws IOException {
+		reset();
+		stop();
+		closeConnection();
+	}
+}
